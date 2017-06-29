d663a0410fba4c9e90f708d57c5b378db84811a2
elsharko1 <elsharko1@gmail.com>
2017-06-29 19:26:27 -0400

    second commit

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,9 @@ public class Main {
         Scanner scan = new Scanner(System.in);
 
         int sidesDice = 0;
+        String choice = "y";
 
+        while (choice.equalsIgnoreCase("y")) {
         System.out.println("Welcome to Grand Circus Casino\n\n");
         System.out.println("How many sides are on the dice: ");
         sidesDice = scan.nextInt();
@@ -20,6 +22,12 @@ public class Main {
         System.out.println(rollDice(sidesDice) + "\n" + rollDice(sidesDice));
 
         // write your code here
+            System.out.println("Continue? (y/n)");
+            scan.nextLine();
+            choice = scan.nextLine();
+
+        }
+        System.out.println("Goodbye");
     }
     // new method called rollDice
     // in parenthesis calling the specific integer we want
